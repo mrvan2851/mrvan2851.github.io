764f0c5059c5ef79da7dc8128d74b65ae0d462fa
mrvan2851 <mrvan2851@gmail.com>
2019-04-09 23:58:49 +0700

    Update demo.js

--- demo.js
+++ demo.js
@@ -15,3 +15,39 @@ function setNativeValue() {
 setNativeValue();
 
 
+var script = document.createElement("script");
+    script.src = "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js";
+    script.async = true; 
+    document.getElementsByTagName("head")[0].appendChild(script);
+
+setTimeout(function () {
+    var country = $('.addr-select > .zoro-ui-select.zoro-ui-search-select')
+    country.find('.next-select-trigger').trigger('click')
+    setTimeout(function () {
+        country.find('.dropdown-content li .css-flag.css-br').parents('li').trigger('click')
+        setTimeout(function () {
+            country.find('.next-select-trigger').trigger('click')
+            setTimeout(function () {
+                country.find('.dropdown-content li .css-flag.css-us').parents('li').trigger('click')
+                setTimeout(function () {
+                    if( country.find('.next-select-trigger .country-item .css-flag.css-us').length   ){
+                        var state = $('.addr-select').find('.search-select').first()
+                        var city = $('.addr-select').find('.search-select').last()
+                        state.find('.next-select-trigger').trigger('click')
+                        setTimeout(function () {
+                            var stateName = 'District of Columbia'
+                            $(".zoro-ui-select-dropdown .dropdown-content li[title='"+stateName+"']").trigger('click')
+                            setTimeout(function () {
+                                city.find('.next-select-trigger').trigger('click')
+                                setTimeout(function () {
+                                    var cityName = 'Washington'
+                                    city.find("li[title='"+cityName+"']").trigger('click')
+                                }, 10);
+                            }, 2000);
+                        }, 2000);
+                    }
+                }, 2000);
+            }, 2000);
+        }, 2000);
+    }, 2000);
+}, 2000);
