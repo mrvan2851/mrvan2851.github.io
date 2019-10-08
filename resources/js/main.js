f628e28bd4785bced5fa648bfbbca101bfb1da6b
mrvan2851 <mrvan2851@gmail.com>
2019-10-09 01:04:50 +0700

    update

--- resources/js/main.js
+++ resources/js/main.js
@@ -4,7 +4,7 @@ function Main(){
 		var header = $('header.header');
 		$(window).on('load scroll', function(){
 			var scroll = $(window).scrollTop();
-			if (scroll >= 100){
+			if (scroll >= $('.header-top').height()){
 				header.addClass('header-fixed');
 			}else{
 				header.removeClass('header-fixed');
@@ -48,14 +48,44 @@ function Main(){
 			}
 		})
 	}
+	this.initModalVideo = function(element){
+		var $videoSrc;
+		var modal = $(element)
+		var video = modal.find('iframe')
+		var btn = $('.btn-modal-video')
+		btn.click(function () {
+			$videoSrc = $(this).data("src");
+			modal.modal('show')
+		});
+		modal.on('shown.bs.modal', function (e) {
+			video.attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
+		})
+		modal.on('hide.bs.modal', function (e) {
+			video.attr('src', '');
+		})
+	}
+	this.initModalArticle = function(element){
+		var modal = $(element)
+		var btn = $('.btn-modal-article')
+		btn.click(function () {
+			var box = $(this).data("box");
+			$(box).addClass('is-active')
+			modal.modal('show')
+		});
+		modal.on('hide.bs.modal', function (e) {
+			modal.find('.box').removeClass('is-active')
+		})
+	}
     this.init = function(){
 		this.sticky();
 		smartScroll.init({
 			activeClass:"is-active",
 			offset: 60
 		})
+		this.initModalVideo('#modal-popup-video');
+		this.initModalArticle('#modal-popup-article');
 		this.mobileNavbar();
-		// this.initWow();
+		
     }
     return this;
 }
