b99b7ff0a7b9884fd8683ba2f67d90fcc94fa5e7
mrvan2851 <mrvan2851@gmail.com>
2020-07-13 16:40:58 +0700

    update

--- landing/js/main.js
+++ landing/js/main.js
@@ -4,9 +4,11 @@ function SocialPublish() {
         vm.header()
         vm.navbar()
         vm.collapse('#autopost-accordion')
-        vm.carousel('#carousel')
+		vm.carousel('#carousel')
+		vm.ShCarousel('#sh-testimonial')
         vm.autopost('#autopost-carousel')
-        vm.form_register('#modal-register-form')
+		vm.form_register('#modal-register-form')
+		vm.ShDiscover('#ist-items-discover')
     }
     vm.collapse = function(root_element) {
         var root = $(root_element)
@@ -131,6 +133,35 @@ function SocialPublish() {
                 prevArrow: '<button class="testimonial-navbar-prev"></button>'
             });
         }
+	}
+	vm.ShCarousel = function(element) {
+        if ($(element).length) {
+            $(element).slick({
+                infinite: true,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                adaptiveHeight: false,
+                autoplay: true,
+				autoplaySpeed: 2000,
+				fade:true,
+                prevArrow: '<button class="prev-arrow"><svg aria-hidden="true" focusable="false" data-prefix="far" data-icon="chevron-left" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512" class="svg-inline--fa fa-chevron-left fa-w-8"><path fill="currentColor" d="M231.293 473.899l19.799-19.799c4.686-4.686 4.686-12.284 0-16.971L70.393 256 251.092 74.87c4.686-4.686 4.686-12.284 0-16.971L231.293 38.1c-4.686-4.686-12.284-4.686-16.971 0L4.908 247.515c-4.686 4.686-4.686 12.284 0 16.971L214.322 473.9c4.687 4.686 12.285 4.686 16.971-.001z" class=""></path></svg></button>',
+                nextArrow: '<button class="next-arrow"><svg aria-hidden="true" focusable="false" data-prefix="far" data-icon="chevron-right" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512" class="svg-inline--fa fa-chevron-right fa-w-8"><path fill="currentColor" d="M24.707 38.101L4.908 57.899c-4.686 4.686-4.686 12.284 0 16.971L185.607 256 4.908 437.13c-4.686 4.686-4.686 12.284 0 16.971L24.707 473.9c4.686 4.686 12.284 4.686 16.971 0l209.414-209.414c4.686-4.686 4.686-12.284 0-16.971L41.678 38.101c-4.687-4.687-12.285-4.687-16.971 0z" class=""></path></svg></button>'
+            });
+        }
+	}
+	vm.ShDiscover = function(element) {
+        if ($(element).length && $(window).width() <= 768) {
+            $(element).slick({
+                infinite: true,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                adaptiveHeight: false,
+                autoplay: true,
+				autoplaySpeed: 2000,
+				arrows : false,
+				dots: true,
+			});
+        }
     }
     vm.autopost = function(element) {
         if ($(element).length) {
@@ -152,7 +183,6 @@ function SocialPublish() {
                 }
             })
         }
-
     }
     vm.form_register = function(element) {
         var form = $(element)
